5b07fa117a65ee301502c2f84d1c3c310d3f8102
Wes <wmcmillan1012@gmail.com>
2020-09-10 15:20:06 -0700

    updated pseudo code

--- script.js
+++ script.js
@@ -10,15 +10,24 @@ function writePassword() {
 
 }
 
-//TODO- Create the generatePassword function that displays final password after user responds to prompts.
-//TODO- Need prompts to ask user for password criteria.
+//TODO- Create the generatePassword function that displays final password after user responds to prompts. All code exists within this function.
+
+//TODO- Create prompts to ask user for password criteria.
   //TODO- Ask user for desired password length between 8 and 128 characters and save it to a VAR.
   //TODO- Ask user if they want uppercase
   //TODO- Ask user if they want lowercase
   //TODO- Ask user if they want numbers
   //TODO- Ask user if they want special characters
-  //TODO- Add a validation to make sure at least one prompt was selected.
+  //TODO- Create arrays with all needed characters (lowercase, uppercase, numbers, special characters)
+  //TODO- create a new var that holds all the chosen characters
+  //TODO- Create a conditional situation that saves the chosen character from our confirms to a new collection (using an if statement)
 
+//TODO- Select the specific number of characters randomly from our final collection based on the users answer to how long they want the length of their password to be
+  //can use Math.floor(Math.random()) to get a random number
+    //for loop to the length of the selected character variable
+
+//TODO- Add a validation to make sure at least one prompt was selected.
+//TODO- Add validation that confirms password is between 8-128 characters.
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
