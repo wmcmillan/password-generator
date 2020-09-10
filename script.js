5f634ec7f6e26a15a9f45fdfcbb6712dc5e45e92
Wes <wmcmillan1012@gmail.com>
2020-09-10 14:52:04 -0700

    updated pseudo code

--- script.js
+++ script.js
@@ -13,8 +13,11 @@ function writePassword() {
 //TODO- Create the generatePassword function that displays final password after user responds to prompts.
 //TODO- Need prompts to ask user for password criteria.
   //TODO- Ask user for desired password length between 8 and 128 characters and save it to a VAR.
-  //TODO- Ask user for desired character types, such as lowercase, uppercase, numbers, and/or special characters with a confirm.
-
+  //TODO- Ask user if they want uppercase
+  //TODO- Ask user if they want lowercase
+  //TODO- Ask user if they want numbers
+  //TODO- Ask user if they want special characters
+  //TODO- Add a validation to make sure at least one prompt was selected.
 
 
 // Add event listener to generate button
